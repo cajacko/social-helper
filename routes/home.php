4b5f9e181dd39ebf2ddf9e2cddfdfba3b1290a98
Charlie Jackson <charlie@charliejackson.com>
2016-05-19 16:46:56 +0100

    Added gulp file

--- routes/home.php
+++ routes/home.php
@@ -11,6 +11,61 @@ $user = new User($db, $twitter);
     $template_path .= 'home';
     // print_r($_SESSION);
     // session_destroy();
+
+    $vars['tags'] = array(
+        array('tag' => 'iot', 'id' => 234),
+        array('tag' => 'internetofthings', 'id' => 152),
+        array('tag' => 'entrepreneurship', 'id' => 456),
+        array('tag' => 'startup', 'id' => 223),
+        array('tag' => 'improv', 'id' => 789),
+    );
+
+    $vars['links'] = array(
+        array(
+            'url' => 'http://apple.com',
+            'id' => 3890,
+            'tweets' => array(
+                array(
+                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
+                    'user' => 'Viki Bell',
+                    'username' => 'Vikiibell',
+                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
+                    'date' => 'May 18, 2016',
+                    'id' => 49863,
+                ),
+                array(
+                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
+                    'user' => 'Viki Bell',
+                    'username' => 'Vikiibell',
+                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
+                    'date' => 'May 18, 2016',
+                    'id' => 49863,
+                ),
+            ),
+        ),
+        array(
+            'url' => 'http://apple.com',
+            'id' => 4521,
+            'tweets' => array(
+                array(
+                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
+                    'user' => 'Viki Bell',
+                    'username' => 'Vikiibell',
+                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
+                    'date' => 'May 18, 2016',
+                    'id' => 49863,
+                ),
+                array(
+                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
+                    'user' => 'Viki Bell',
+                    'username' => 'Vikiibell',
+                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
+                    'date' => 'May 18, 2016',
+                    'id' => 49863,
+                ),
+            ),
+        ),
+    );
 /*} else {
     header('Location: /twitter/login');
     exit;
--- routes/index.php
+++ routes/index.php
@@ -14,6 +14,7 @@ session_start();
 
 $request = $_GET['url'];
 $request = explode('/', $request);
+$vars = array();
 
 if(isset($request[0])) {
     switch($request[0]) {
@@ -31,62 +32,6 @@ if(isset($request[0])) {
 }
 
 ob_start("ob_gzhandler");
-
-$vars = array(
-    'tags' => array(
-        array('tag' => 'iot', 'id' => 234),
-        array('tag' => 'internetofthings', 'id' => 152),
-        array('tag' => 'entrepreneurship', 'id' => 456),
-        array('tag' => 'startup', 'id' => 223),
-        array('tag' => 'improv', 'id' => 789),
-    ),
-    'links' => array(
-        array(
-            'url' => 'http://apple.com',
-            'id' => 3890,
-            'tweets' => array(
-                array(
-                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
-                    'user' => 'Viki Bell',
-                    'username' => 'Vikiibell',
-                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
-                    'date' => 'May 18, 2016',
-                    'id' => 49863,
-                ),
-                array(
-                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
-                    'user' => 'Viki Bell',
-                    'username' => 'Vikiibell',
-                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
-                    'date' => 'May 18, 2016',
-                    'id' => 49863,
-                ),
-            ),
-        ),
-        array(
-            'url' => 'http://apple.com',
-            'id' => 4521,
-            'tweets' => array(
-                array(
-                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
-                    'user' => 'Viki Bell',
-                    'username' => 'Vikiibell',
-                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
-                    'date' => 'May 18, 2016',
-                    'id' => 49863,
-                ),
-                array(
-                    'tweet' => 'I&#39;ve had pizza and red wine for every dinner so far this week. How am I going to cope this evening?! 🍷🍕',
-                    'user' => 'Viki Bell',
-                    'username' => 'Vikiibell',
-                    'url' => 'https://twitter.com/Vikiibell/status/732988506092507137',
-                    'date' => 'May 18, 2016',
-                    'id' => 49863,
-                ),
-            ),
-        ),
-    ),
-);
 
 $template = $twig->loadTemplate($template_path . '.twig');
 $content = $template->render(array('vars' => $vars));
