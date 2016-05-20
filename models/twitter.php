564e0e3e77e9fa4da842c1795f5ddaf6fded2524
Charlie Jackson <charlie@charliejackson.com>
2016-05-20 12:23:39 +0100

    Added add/delete tags functions

--- models/twitter.php
+++ models/twitter.php
@@ -11,12 +11,14 @@ class Twitter {
     private $db;
     private $token;
     private $token_secret;
+    private $connection_type = false;
 
     public function __construct($config) {
         $this->key = $config->twitter->key;
         $this->secret = $config->twitter->secret;
         $this->callback = $config->twitter->callback;
         $this->connection = new TwitterOAuth($this->key, $this->secret);
+        $this->connection_type = 'temp';
     }
 
     public function login_url() {
@@ -40,21 +42,49 @@ class Twitter {
             );
 
             return $this->connection->oauth("oauth/access_token", $params);
-            // $this->token = $access_token['oauth_token'];
-            // $this->token_secret = $access_token['oauth_token_secret'];
-
-            // $_SESSION['oauth_token'] = $this->token;
-            // $_SESSION['oauth_token_secret'] = $this->token_secret;
         } else {
             return false;
         }
+    }
 
-        // $this->connection = new TwitterOAuth(
-        //     $this->key, $this->secret, 
-        //     $this->token, 
-        //     $this->token_secret
-        // );
+    public function set_user_connection() {
+        if('user' == $this->connection_type) {
+            return true;
+        } else {
+            $this->connection = new TwitterOAuth(
+                $this->key, 
+                $this->secret, 
+                $_SESSION['oauth_token'], 
+                $_SESSION['oauth_token_secret']
+            );
 
-        // return $access_token;
+            $this->connection_type = 'user';
+            return true;
+        }
+    }
+
+    public function set_app_connection() {
+        if('app' == $this->connection_type) {
+            return true;
+        } else {
+            $result = $this->connection->oauth2('oauth2/token', array('grant_type' => 'client_credentials'));
+            $this->connection_type = 'app';
+            return true;
+        }
+    }
+
+    public function get_tweets() {
+        $this->set_app_connection();
+
+        $tweet_response = $this->connection->get( "search/tweets", array( 
+            'q' => '#iot filter:links -filter:retweets', 
+            "count" => 10, 
+            'exclude_replies' => TRUE,
+            'lang' => 'en',
+            'result_type' => 'recent',
+        ));   
+
+        print_r($tweet_response); exit;
+
     }
 }
