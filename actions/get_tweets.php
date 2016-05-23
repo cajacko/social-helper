2b122ce262d1002656e61d6e662a29c32e762cf6
Charlie Jackson <charlie@charliejackson.com>
2016-05-23 12:16:58 +0100

    Prevent duplicate crons via lock file

--- actions/get_tweets.php
+++ actions/get_tweets.php
@@ -1,13 +1,23 @@
 <?php
 
-set_include_path(__DIR__ . '/..');
+$fp = fopen("get_tweets.txt", "w+");
 
-require_once('models/config.php');
-$config = new Config;
-require_once('models/database.php');
-require_once('models/twitter.php');
+if (flock($fp, LOCK_EX | LOCK_NB)) { // do an exclusive lock
+    set_include_path(__DIR__ . '/..');
 
-$twitter = new Twitter($db, $config->config);
-$twitter->save_new_tweets();
+    require_once('models/config.php');
+    $config = new Config;
+    require_once('models/database.php');
+    require_once('models/twitter.php');
+
+    $twitter = new Twitter($db, $config->config);
+    $twitter->save_new_tweets();
+
+    flock($fp, LOCK_UN); // release the lock
+} else {
+    echo "Couldn't get the lock!";
+}
+
+fclose($fp);
 
 ?>
