5ae73bf7ce2188ceb7ac14730c1c1fcc55ef8e2d
Charlie Jackson <charlie@charliejackson.com>
2017-01-16 17:33:30 +0000

    Added twitter oauth flow

--- web/index.php
+++ web/index.php
@@ -1,1 +1,21 @@
-Hello
+<?php
+
+session_start();
+
+require 'vendor/autoload.php';
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+define('CONSUMER_KEY', getenv('CONSUMER_KEY'));
+define('CONSUMER_SECRET', getenv('CONSUMER_SECRET'));
+
+$access_token = $_SESSION['access_token'];
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+$user = $connection->get("account/verify_credentials");
+
+print_r($user);
